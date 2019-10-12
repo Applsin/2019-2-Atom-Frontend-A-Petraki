a6652fac959173c639efc1fd9bb1a422df069c24
FluffyDumbo <javatechno@mail.ru>
2019-10-12 15:27:14 +0300

    removed cached

--- web-components/src/index.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/extensions */
-import './index.css';
-
-import './components/FormInput.js';
-import './components/MessageForm.js';
-import './components/Message.js';
-import './components/Header.js';
