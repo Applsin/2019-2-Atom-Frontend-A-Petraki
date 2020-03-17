c0af14be79533e35c3e360965c39781c5fb46aed
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-17 06:41:13 +0000

    Deploy Applsin/2019-2-Atom-Frontend-A-Petraki to github.com/Applsin/2019-2-Atom-Frontend-A-Petraki.git:gh-pages

---
+++ precache-manifest.55c71c497f24fd598427ebb8e0117ab7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0462e4e756263ccb3f932ff06aab095",
+    "url": "/2019-2-Atom-Frontend-A-Petraki/index.html"
+  },
+  {
+    "revision": "f1efbb9a49e697daa45b",
+    "url": "/2019-2-Atom-Frontend-A-Petraki/static/css/main.41352148.chunk.css"
+  },
+  {
+    "revision": "1d66a338fc008019b1ed",
+    "url": "/2019-2-Atom-Frontend-A-Petraki/static/js/2.a0a32182.chunk.js"
+  },
+  {
+    "revision": "f1efbb9a49e697daa45b",
+    "url": "/2019-2-Atom-Frontend-A-Petraki/static/js/main.47988a41.chunk.js"
+  },
+  {
+    "revision": "a86359181989e9d7355e",
+    "url": "/2019-2-Atom-Frontend-A-Petraki/static/js/runtime-main.b48a78a9.js"
+  }
+]);
